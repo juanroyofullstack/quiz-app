edd8ba23d2f629517ca03bc9d2c9ac5690b33f79
Juan Royo de Barrio <juanroyofullstack@gmail.com>
2024-06-04 21:39:34 +0200

    add: redirect after hitting loggin

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { FormEvent } from 'react';
+import { useRouter } from 'next/navigation';
 
 import {
     login
@@ -9,14 +10,16 @@ import { useAppDispatch } from '@/lib/hooks';
 
 export default function Page() {
     const dispatch = useAppDispatch();
+    const router = useRouter();
 
     function onSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
 
         const formData = new FormData(event.currentTarget);
-        const inputValue = formData.get('name');
+        const inputValue = formData.get('name')?.toString() ?? '';
 
-        return dispatch(login({ name: inputValue, isLoggedIn: true }));
+        dispatch(login({ name: inputValue, isLoggedIn: true }));
+        router.push('/game');
     }
 
     return (
@@ -37,7 +40,7 @@ export default function Page() {
                                 placeholder=" "
                             />
                             <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-                  Name
+                      Name
                             </label>
                         </div>
                     </div>
@@ -46,7 +49,7 @@ export default function Page() {
                         type="submit"
                         data-ripple-light="true"
                     >
-              Login
+                      Login
                     </button>
                 </form>
             </div>
