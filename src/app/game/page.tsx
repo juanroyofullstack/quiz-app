e78dc4fd0912d8aedb93341f885116d1938f569a
Juan Royo de Barrio <juanroyofullstack@gmail.com>
2025-03-28 18:03:41 +0100

    add: logout button

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -7,7 +7,12 @@ import { Fallback } from '@/app/components/FallbackComponent';
 import { QuestionsContainer } from '@/app/containers/QuestionsContainer';
 import { CountProvider } from '@/app/lib/context/countContext';
 import { useData } from '@/app/lib/hooks/useData';
+import { useLocalStorage } from '@/app/lib/hooks/useLocalStorage';
 import { reloadGame } from '@/lib/features/gameStatusSlice';
+import type { userState } from '@/lib/features/userSlice';
+import {
+    logout
+} from '@/lib/features/userSlice';
 import { useAppDispatch, useAppSelector } from '@/lib/hooks';
 
 export default function Page () {
@@ -17,19 +22,21 @@ export default function Page () {
     const isNotLoadingAndHasError = !loading && error;
     const dispatch = useAppDispatch();
     const router = useRouter();
+    const [, setValue]: [userState, (value: userState) => void] = useLocalStorage<userState>('game', { name: '', isLoggedIn: false }, 'cookie', -1);
 
-    // useEffect(() => {
-    //     if(!isUserLoggedIn) {
-    //         router.push('/');
-    //     }
-    // }, [isUserLoggedIn, router]);
+    const logOut = async () => {
+        await dispatch(logout());
+        await setValue({ name: '', isLoggedIn: false });
+        await dispatch(reloadGame());
+        return router.push('/');
+    };
 
     if(isNotLoadingAndHasError) {
         return <Fallback errorMessage={'test'} refreshOnClick={() => dispatch(reloadGame())}/>;
     }
-
     return (
         <div className='GameContainer h-full'>
+            {isUserLoggedIn && isNotLoadingAndHasData && <button onClick={() => logOut()}>Log Out</button>}
             <CountProvider>
                 {isNotLoadingAndHasData && <QuestionsContainer questions={data}/>}
             </CountProvider>
