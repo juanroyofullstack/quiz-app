6650054e45ff2abe4151eaeb216a30c4d8651588
Juan Royo de Barrio <juanroyofullstack@gmail.com>
2025-04-04 20:41:11 +0200

    add: middleware protection and url param

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { FormEvent } from 'react';
 import { useRouter } from 'next/navigation';
 
 import { useLocalStorage } from '@/app/lib/hooks/useLocalStorage';
@@ -13,20 +13,18 @@ import { useAppDispatch } from '@/lib/hooks';
 
 export default function Page() {
     const dispatch = useAppDispatch();
-    const [name, setName] = useState<string>('');
     const router = useRouter();
     const [, setValue]: [userState, (value: userState) => void] = useLocalStorage<userState>('game', { name: '', isLoggedIn: false }, 'cookie', 7);
 
-    const handleChange = (event : React.ChangeEvent<HTMLInputElement>) => {
-        setName(event.target.value);
-    };
-
-    const onSendInfo = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        dispatch(login({ name: name, isLoggedIn: true }));
+
+        const formData = new FormData(event.currentTarget);
+        const inputName = formData.get('name')?.toString() ?? '';
+        dispatch(login({ name: inputName, isLoggedIn: true }));
         dispatch(loadingGame());
-        await setValue({ name: name, isLoggedIn: true });
-        return router.push('/game');
+        await setValue({ name: inputName, isLoggedIn: true });
+        return router.push(`/game?name=${encodeURIComponent(inputName)}`);
     };
 
     return (
@@ -41,14 +39,13 @@ export default function Page() {
                 <p className="mt-1 block font-sans text-base font-normal leading-relaxed text-gray-700 antialiased">
             After you enter your details the game will start.
                 </p>
-                <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+                <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96" onSubmit={onSubmit}>
                     <div className="mb-4 flex flex-col gap-6">
                         <div className="relative h-11 w-full min-w-[200px]">
                             <input
                                 className="peer h-full w-full rounded-md border border-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                                 name="name"
                                 placeholder=" "
-                                onChange={handleChange}
                             />
                             <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                       Name
@@ -57,9 +54,8 @@ export default function Page() {
                     </div>
                     <button
                         className="mt-6 block w-full select-none rounded-lg bg-pink-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                        type="submit"
                         data-ripple-light="true"
-                        disabled={name.length < 1}
-                        onClick={onSendInfo}
                     >
                       Login
                     </button>
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,10 +4,12 @@ import { NextResponse } from "next/server";
 export async function middleware (req: NextRequest) {
     const { pathname } = req.nextUrl;
     const user = await req.cookies.get('game')?.value;
+    const urlParams = new URLSearchParams(req.nextUrl.search);
+    const name = urlParams.get('name');
 
     if (pathname.startsWith('/game')) {
         const userData = user ? JSON.parse(user) : null;
-        if (!userData || !userData.isLoggedIn) {
+        if (!userData || !userData.isLoggedIn || userData.name !== name) {
             return NextResponse.redirect(new URL('/', req.url));
         }
         return NextResponse.next();
