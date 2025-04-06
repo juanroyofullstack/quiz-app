04b31080df6b737fea1348fc0977210e86d443e4
Juan Royo de Barrio <juanroyofullstack@gmail.com>
2025-04-06 17:35:25 +0200

    add: identation rules

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -11,23 +11,28 @@ import { useData } from '@/app/lib/hooks/useData';
 import { useLocalStorage } from '@/app/lib/hooks/useLocalStorage';
 import { loadingGame, reloadGame } from '@/lib/features/gameStatusSlice';
 import type { userState } from '@/lib/features/userSlice';
-import {
-    logout
-} from '@/lib/features/userSlice';
+import { logout } from '@/lib/features/userSlice';
 import { useAppDispatch, useAppSelector } from '@/lib/hooks';
 
-export default function Page () {
+export default function Page() {
     const { data, error, loading } = useData();
-    const isUserLoggedIn = useAppSelector(state => state.user.isLoggedIn);
-    const countStatus = useAppSelector(state => state.game.countDownStatus);
+    const isUserLoggedIn = useAppSelector((state) => state.user.isLoggedIn);
+    const countStatus = useAppSelector((state) => state.game.countDownStatus);
 
     const isNotLoadingAndHasData = !loading && data.length > 0;
-    const isNotLoadingHasDataAndCountIsOver = !loading && data.length > 0 && !countStatus;
+    const isNotLoadingHasDataAndCountIsOver =
+        !loading && data.length > 0 && !countStatus;
     const isNotLoadingAndHasError = !loading && error;
 
     const dispatch = useAppDispatch();
     const router = useRouter();
-    const [, setValue]: [userState, (value: userState) => void] = useLocalStorage<userState>('game', { name: '', isLoggedIn: false }, 'cookie', -1);
+    const [, setValue]: [userState, (value: userState) => void] =
+        useLocalStorage<userState>(
+            'game',
+            { name: '', isLoggedIn: false },
+            'cookie',
+            -1,
+        );
 
     useEffect(() => {
         dispatch(loadingGame());
@@ -40,18 +45,28 @@ export default function Page () {
         return router.push('/');
     };
 
-    if(isNotLoadingAndHasError) {
-        return <Fallback errorMessage={'test'} refreshOnClick={() => dispatch(loadingGame())}/>;
+    if (isNotLoadingAndHasError) {
+        return (
+            <Fallback
+                errorMessage={'test'}
+                refreshOnClick={() => dispatch(loadingGame())}
+            />
+        );
     }
 
     return (
-        <div className='GameContainer h-full'>
-            {isUserLoggedIn && isNotLoadingAndHasData && <button onClick={() => logOut()}>Log Out</button>}
+        <div className="GameContainer h-full">
+            {isUserLoggedIn && isNotLoadingAndHasData && (
+                <button onClick={() => logOut()}>Log Out</button>
+            )}
             <CountProvider>
                 {loading && <div>Loading...</div>}
-                {!loading && data.length > 0 &&
-                 <CountDownComponent count={3} />}
-                {isNotLoadingHasDataAndCountIsOver && <QuestionsContainer questions={data}/>}
+                {!loading && data.length > 0 && (
+                    <CountDownComponent count={3} />
+                )}
+                {isNotLoadingHasDataAndCountIsOver && (
+                    <QuestionsContainer questions={data} />
+                )}
             </CountProvider>
         </div>
     );
